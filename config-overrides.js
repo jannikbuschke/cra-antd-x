12a27c062f6c51eb4a3abeb421ed51a125832b5b
Jannik Buschke <jannik.buschke@gmail.com>
2020-04-25 10:23:39 +0200

    fix babel imports

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,20 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
-const { addReactRefresh } = require('customize-cra-react-refresh')
+const { override, fixBabelImports, addLessLoader } = require("customize-cra")
+const { addReactRefresh } = require("customize-cra-react-refresh")
 
 module.exports = override(
-    fixBabelImports('antd', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-    }),
-    fixBabelImports('formik-antd', {
-        libraryName: 'formik-antd',
-        style: true,
-        libraryDirectory: 'es'
-    }),
-    addReactRefresh({ disableRefreshCheck: true }),
-    // addLessLoader({
-    //     javascriptEnabled: true,
-    //     modifyVars: { '@primary-color': 'rgb(51,122,183)' },
-    // }),
-);
+  fixBabelImports("antd", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: "css",
+  }),
+  fixBabelImports("formik-antd", {
+    libraryName: "formik-antd",
+    style: "css",
+    libraryDirectory: "es",
+  }),
+  addReactRefresh({ disableRefreshCheck: true }),
+  // addLessLoader({
+  //     javascriptEnabled: true,
+  //     modifyVars: { '@primary-color': 'rgb(51,122,183)' },
+  // }),
+)
